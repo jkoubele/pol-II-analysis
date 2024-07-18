088056a02dae782276268583bc857422cbc1d9ed
Jakub <koubele.jakub@gmail.com>
2024-07-18 15:10:55 +0200

    Add code for featureCount.

---
+++ pipeline/batch_feature_counts.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Function to display usage information
+usage() {
+    echo "Usage: $0 -i <input_folder> -o <output_folder> -g <genome_folder>"
+    echo " -a <annotation_gtf_file_name> -s <strandedness>"
+    echo "[-d <docker_image_path>] [-l <slurm_log_folder>] [-L]"
+    exit 1
+}
+
+script_directory="$(cd "$(dirname "$0")" && pwd)"
+repository_path="$(dirname "$script_directory")"
+
+# Variables to hold arguments
+input_folder=""
+output_folder=""
+genome_folder=""
+annotation_gtf_file_name=""
+strandedness=""
+run_locally=false
+docker_image_path="$repository_path"/docker_images/bioinfo_tools.tar
+slurm_log_folder="$repository_path"/slurm_logs
+
+# Parse command line arguments
+while getopts ":i:o:g:d:a:s:l:L" opt; do
+    case ${opt} in
+        i )
+            input_folder=$OPTARG
+            ;;
+        o )
+            output_folder=$OPTARG
+            ;;
+        g )
+            genome_folder=$OPTARG
+            ;;
+        d )
+            docker_image_path=$OPTARG
+            ;;
+        a )
+            annotation_gtf_file_name=$OPTARG
+            ;;
+        s )
+            strandedness=$OPTARG
+            ;;
+        l )
+            slurm_log_folder=$OPTARG
+            ;;
+        L )
+            run_locally=true
+            ;;
+        \? )
+            echo "Invalid option: $OPTARG" 1>&2
+            usage
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            usage
+            ;;
+    esac
+done
+
+# Check if mandatory arguments are provided
+if [ -z "$input_folder" ] || [ -z "$output_folder" ] || [ -z "$genome_folder" ] \
+|| [ -z "$annotation_gtf_file_name" ] || [ -z "$strandedness" ]; then
+    echo "Error: Missing mandatory arguments"
+    usage
+fi
+
+# Create output folder if it doesn't exist
+mkdir "$output_folder" -p
+
+for sub_folder in "$input_folder"/*; do
+  sample_name=$(basename "$sub_folder")
+  if [ "$run_locally" = true ]; then
+    echo "Processing sample $sample_name"
+    sh "$repository_path"/scripts/feature_counts.sh -i "$sub_folder" -o "$output_folder"/"$sample_name" \
+    -d "$docker_image_path" -g "$genome_folder" -a "$annotation_gtf_file_name" -s "$strandedness"
+  else
+    echo "Submitting sample $sample_name"
+    sbatch --output="$slurm_log_folder"/%j_%x.log --error="$slurm_log_folder"/%j_%x.err \
+    "$repository_path"/scripts/feature_counts.sh -i "$sub_folder" -o "$output_folder"/"$sample_name" \
+    -d "$docker_image_path" -g "$genome_folder" -a "$annotation_gtf_file_name" -s "$strandedness"
+  fi
+done
---
+++ scripts/feature_counts.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#SBATCH --job-name=feature_counts
+#SBATCH --partition=all
+#SBATCH --ntasks=12
+
+# Function to display usage information
+usage() {
+    echo "Usage: $0 -i <input_folder> -o <output_folder> -d <docker_image_path>"
+    echo " -g <genome_folder> -a <annotation_gtf_file_name> -s <strandedness>"
+    exit 1
+}
+
+# Variables to hold arguments
+input_folder=""
+output_folder=""
+docker_image_path=""
+genome_folder=""
+annotation_gtf_file_name=""
+strandedness=""
+
+# Parse command line arguments
+while getopts ":i:o:d:a:g:s:" opt; do
+    case ${opt} in
+        i )
+            input_folder=$OPTARG
+            ;;
+        o )
+            output_folder=$OPTARG
+            ;;
+        d )
+            docker_image_path=$OPTARG
+            ;;
+        g )
+            genome_folder=$OPTARG
+            ;;
+        a )
+            annotation_gtf_file_name=$OPTARG
+            ;;
+        s )
+            strandedness=$OPTARG
+            ;;
+        \? )
+            echo "Invalid option: $OPTARG" 1>&2
+            usage
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            usage
+            ;;
+    esac
+done
+
+# Check if mandatory arguments are provided
+if [ -z "$input_folder" ] || [ -z "$output_folder" ] || [ -z "$docker_image_path" ] \
+|| [ -z "$genome_folder" ] || [ -z "$annotation_gtf_file_name" ] || [ -z "$strandedness" ]; then
+    echo "Error: Missing mandatory arguments"
+    usage
+fi
+
+# Check if the docker image is available, and load it from disk if it's not
+if ! docker images --format "{{.Repository}}" | grep -q "^bioinfo_tools$"; then
+    docker load -i "$docker_image_path"
+fi
+
+# Create output folder if it doesn't exist
+mkdir "$output_folder" -p
+
+# Run featureCounts
+docker run --rm -v "$input_folder":/input_folder -v "$output_folder":/output_folder \
+-v "$genome_folder":/genome_folder --security-opt seccomp=unconfined \
+bioinfo_tools /bin/sh -c "featureCounts -p --countReadPairs -s $strandedness -T 12 \
+-t gene -a $genome_folder/$annotation_gtf_file_name \
+-o /output_folder/feature_counts.tsv \
+/input_folder/Aligned.sortedByCoord.out.bam;
+chmod 777 -R /output_folder"
